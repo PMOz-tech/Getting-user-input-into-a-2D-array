ceaa6a702b6031c702fbfccf9dd55dabe3e9cd62
PMOz-tech <ozon.oghenemaro@gmail.com>
2021-11-08 08:22:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            int[,] multiDim = new int[4, 4];
+            for (int i = 0; i < multiDim.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiDim.GetLength(1); j++)
+                {
+
+                    multiDim[i, j] = Convert.ToInt32(Console.In.ReadLine());
+                    //Console.WriteLine(multiDim[i, j]);
+                }
+
+            }
+
+            for (int i = 0; i < multiDim.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiDim.GetLength(1); j++)
+                {
+                    Console.WriteLine(multiDim[i, j]);
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
